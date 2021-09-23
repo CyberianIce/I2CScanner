6bae2f3262824dedaf0729e320c90f8e3db55e14
Vladimir Radakovic <cyberianice@gmail.com>
2021-09-23 02:09:22 +0200

    Create I2CScanner.ino

---
+++ I2CScanner/I2CScanner.ino
@@ -0,0 +1,31 @@
+#include <Wire.h>
+
+void setup() {
+  Wire.begin();
+  Serial.begin(9600);
+  while (!Serial);
+  Serial.println("I2C Scanner");
+}
+ 
+void loop() {
+  byte error, address;
+  int nDevices;
+  Serial.println("Scanning...");
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address<16) Serial.print("0");
+      Serial.println(address, HEX);
+      nDevices++;
+    } else if (error==4) {
+      Serial.print("Unknown error at address 0x");
+      if (address<16) Serial.print("0");
+      Serial.println(address, HEX);
+    }
+  }
+  Serial.println(nDevices == 0 ? "No I2C devices found" : "done");
+  delay(3000);
+}
